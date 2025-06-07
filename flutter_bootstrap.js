439098ed0fd68dff58b88163d8a38ee6da6a11fc
a.liubavitski <a.liubavitski@olx.pl>
2025-06-07 15:34:59 +0200

    Fix image picker and add missing UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2302569840"
+    serviceWorkerVersion: "3223304497"
   }
 });
